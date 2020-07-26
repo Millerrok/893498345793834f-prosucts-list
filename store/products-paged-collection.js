5bace081afc8998b6d02ce57befe67cdb212b641
Maksym Sorochuk <maksim.sorochuk@comodo.com>
2020-07-26 14:05:31 +0300

    make collection without constructor

--- store/products-paged-collection.js
+++ store/products-paged-collection.js
@@ -1,5 +1,34 @@
-import makePagedCollectionStore from "./paged-collection";
 import ProductStore from "./product";
+import {types} from "mobx-state-tree";
+import PaginationStore from "./pagination";
+import {chunkMap} from "../utils";
 
-const ProductsPagedCollection = makePagedCollectionStore(ProductStore);
+const updateMapByObject = (map, obj) => {
+    for (let [key, value] of obj) {
+        map.set(key, value)
+    }
+};
+
+const ProductsPagedCollection = types
+    .model('PagedCollectionStore', {
+        pagination: types.optional(PaginationStore, {}),
+        pages: types.map(types.array(ProductStore)),
+    })
+    .actions(self => ({
+        updatePagesMap(pages) {
+            updateMapByObject(self.pages, pages)
+        },
+        updateData(data) {
+            const pagesMap = chunkMap(data, self.pagination.resultPerPage);
+
+            self.updatePagesMap(pagesMap);
+            self.pagination.updateTotalPages(self.pages.size);
+        }
+    }))
+    .views(self => ({
+        get data() {
+            return self.pages.get(self.pagination.currentPage) || [];
+        }
+    }));
+
 export default ProductsPagedCollection;

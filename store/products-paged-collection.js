238f2f25a574966e973c4f154d9ecfb30a5bffd2
Maksym Sorochuk <maksim.sorochuk@comodo.com>
2020-07-27 05:12:47 +0300

    remove untraked files

--- store/products-paged-collection.js
+++
@@ -1,5 +0,0 @@
-import makePagedCollectionStore from "./paged-collection";
-import ProductStore from "./product";
-
-const ProductsPagedCollection = makePagedCollectionStore(ProductStore);
-export default ProductsPagedCollection;
